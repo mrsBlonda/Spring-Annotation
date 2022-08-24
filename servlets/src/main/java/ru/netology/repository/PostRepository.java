0722e1cafcc9071e018e6cfec56b24d54d78713b
mrsBlonda <Anna89851004453@yandex.ru>
2022-08-24 23:47:33 +0300

    ДЗ по сервлетам

---
+++ servlets/src/main/java/ru/netology/repository/PostRepository.java
@@ -0,0 +1,50 @@
+package ru.netology.repository;
+
+import ru.netology.exception.NotFoundException;
+import ru.netology.model.Post;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+
+// Stub
+public class PostRepository {
+  final ConcurrentHashMap<Long, String> map = new ConcurrentHashMap<>();
+  public List<Post> all() {
+    List<Post> posts = new ArrayList<>();
+    Post post;
+    for (var set : map.entrySet()) {
+      post = new Post(set.getKey(), set.getValue());
+      posts.add(post);
+    }
+    return posts;
+  }
+
+  public Optional<Post> getById(long id) {
+    Post post = null;
+    if (map.containsKey(id)) {
+      post = new Post(id, map.get(id));
+    }
+    final var result = Optional.ofNullable(post);
+    return result;
+  }
+
+  public Post save(Post post) {
+    long key = 1;
+    if (post.getId() == 0) {
+      while (map.containsKey(key)) {
+        key++;
+      }
+      map.put(key, post.getContent());
+      key++;
+    } else {
+      map.put(post.getId(), post.getContent());
+    }
+    return post;
+  }
+
+  public void removeById(long id) {
+    if (map.containsKey(id)) {
+      map.remove(id);
+    }
+  }
+}
