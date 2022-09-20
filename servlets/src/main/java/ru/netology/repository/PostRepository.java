582b72a21e5c2b9d739ca4f5ee96a50e47f9e0b9
Прохватаева Анна <Anna89851004453@yandex.ru>
2022-09-20 23:19:58 +0300

    Изменена ветка мастер

--- servlets/src/main/java/ru/netology/repository/PostRepository.java
+++ servlets/src/main/java/ru/netology/repository/PostRepository.java
@@ -10,6 +10,8 @@ import java.util.concurrent.ConcurrentHashMap;
 @Component
 public class PostRepository {
   final ConcurrentHashMap<Long, String> map = new ConcurrentHashMap<>();
+  final AtomicLong key = new AtomicLong(1);
+
   public List<Post> all() {
     List<Post> posts = new ArrayList<>();
     Post post;
@@ -30,7 +32,6 @@ public class PostRepository {
   }
 
   public Post save(Post post) {
-    long key = 1;
     if (post.getId() == 0) {
       while (map.containsKey(key)) {
         key++;
